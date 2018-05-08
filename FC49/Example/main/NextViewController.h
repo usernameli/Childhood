9cd46ec1859a0419f451dbd04da1c49608c0716b
sumingliang <479332917@qq.com>
2018-05-09 01:08:52 +0800

    完成游戏按钮回调

--- FC49/Example/main/NextViewController.h
+++ FC49/Example/main/NextViewController.h
@@ -9,7 +9,30 @@
 #import "QKBasicViewController.h"
 
 @interface NextViewController : QKBasicViewController
-@property (weak, nonatomic) IBOutlet UIView *NesViewContainer;
-- (IBAction)goBack:(id)sender;
+@property (weak, nonatomic) IBOutlet UIView *GameScene;
+- (IBAction)SoundOnOff:(id)sender;
+- (IBAction)Shop:(id)sender;
+//按钮按下操作
+- (IBAction)GameSelectDown:(id)sender;
+- (IBAction)OperADown:(id)sender;
+- (IBAction)OperABDown:(id)sender;
+- (IBAction)OperBDown:(id)sender;
+- (IBAction)GameReset:(id)sender;
+- (IBAction)OperUpDown:(id)sender;
+- (IBAction)OperLeftDown:(id)sender;
+- (IBAction)OperDownDown:(id)sender;
+- (IBAction)OperRightDown:(id)sender;
+- (IBAction)GameStartPauseDown:(id)sender;
+
+//按钮放开
+- (IBAction)GameSelectUp:(id)sender;
+- (IBAction)OperUpUp:(id)sender;
+- (IBAction)OperRightUp:(id)sender;
+- (IBAction)OperLeftUp:(id)sender;
+- (IBAction)OperDownUp:(id)sender;
+- (IBAction)GameStartPauseUp:(id)sender;
+- (IBAction)OperAUp:(id)sender;
+- (IBAction)OperBUp:(id)sender;
+- (IBAction)OperABUp:(id)sender;
 
 @end
--- FC49/Example/main/NextViewController.m
+++ FC49/Example/main/NextViewController.m
@@ -8,9 +8,12 @@
 
 #import "NextViewController.h"
 #import <NESView/NESView.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface NextViewController () <NESViewDelegate>
 @property (nonatomic) NESView* nesView;
+@property (nonatomic) MPVolumeView* volumeView;
+@property (nonatomic, strong) UISlider *volumeViewSlider;
 @property (nonatomic) NSArray<NESKey*>* nesKeys;
 @property (atomic) NSInteger playSpeed;
 @property (nonatomic) NSData* state;
@@ -23,6 +26,15 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    MPVolumeView *volumeView =  [[MPVolumeView alloc] init];
+   
+    volumeView.showsRouteButton = NO;
+    
+    volumeView.showsVolumeSlider = NO;
+    
+    [self.view addSubview:volumeView];
+    _volumeView = volumeView;
+    
     [self initNesView];
 }
 - (void) initNesView {
@@ -33,9 +45,15 @@
     }
     self.nesKeys = keys;
     _playSpeed = 1;
-    _nesView = [[NESView alloc] initWithFrame:CGRectMake(0, 0, 256, 240)];
+    _nesView = [[NESView alloc] initWithFrame:CGRectMake(0, 0, 320, 300)];
     _nesView.delegate = self;
-    [self.NesViewContainer addSubview:_nesView];
+    [self.GameScene addSubview:_nesView];
+    
+    
+    NSURL* romURL = [[NSBundle mainBundle] URLForResource:@"example" withExtension:@"nes"];
+    NSLog(@"rom: %@", romURL);
+    NSData* romData = [NSData dataWithContentsOfFile:romURL.path];
+    [_nesView loadRom:romData];
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -73,8 +91,102 @@
 }
 */
 
-- (IBAction)goBack:(id)sender {
-    [_nesView destroy];
-    [self.navigationController popViewControllerAnimated:NO];
+//- (IBAction)goBack:(id)sender {
+//    [_nesView destroy];
+//    [self.navigationController popViewControllerAnimated:NO];
+//}
+
+
+- (IBAction)OperADown:(id)sender {
+    _nesKeys[0].player1.a = YES;
+}
+
+- (IBAction)OperABDown:(id)sender {
+    _nesKeys[0].player1.a = YES;
+    _nesKeys[0].player1.b = YES;
+}
+
+- (IBAction)OperBDown:(id)sender {
+    _nesKeys[0].player1.b = YES;
+    
+}
+
+- (IBAction)GameReset:(id)sender {
+    
+    [_nesView reset];
+}
+
+- (IBAction)OperUpDown:(id)sender {
+    _nesKeys[0].player1.up = YES;
+}
+
+- (IBAction)OperLeftDown:(id)sender {
+    _nesKeys[0].player1.left = YES;
+}
+
+- (IBAction)OperDownDown:(id)sender {
+    _nesKeys[0].player1.down = YES;
+}
+
+- (IBAction)OperRightDown:(id)sender {
+    _nesKeys[0].player1.right = YES;
+}
+
+- (IBAction)GameStartPauseDown:(id)sender {
+
+    _nesKeys[0].player1.start = YES;
+}
+
+- (IBAction)GameSelectDown:(id)sender {
+    _nesKeys[0].player1.select = YES;
+    
+}
+- (IBAction)GameSelectUp:(id)sender {
+    _nesKeys[0].player1.select = NO;
+}
+
+- (IBAction)OperUpUp:(id)sender {
+    _nesKeys[0].player1.up = NO;
+}
+
+- (IBAction)OperRightUp:(id)sender {
+    _nesKeys[0].player1.right = NO;
+}
+
+- (IBAction)OperLeftUp:(id)sender {
+    _nesKeys[0].player1.left = NO;
+}
+
+- (IBAction)OperDownUp:(id)sender {
+    _nesKeys[0].player1.down = NO;
+}
+
+- (IBAction)GameStartPauseUp:(id)sender {
+    _nesKeys[0].player1.start = NO;
+}
+
+- (IBAction)OperAUp:(id)sender {
+    _nesKeys[0].player1.a = NO;
+}
+
+- (IBAction)OperBUp:(id)sender {
+    _nesKeys[0].player1.b = NO;
+}
+
+- (IBAction)OperABUp:(id)sender {
+    _nesKeys[0].player1.a = NO;
+    _nesKeys[0].player1.b = NO;
+}
+- (IBAction)SoundOnOff:(id)sender {
+    for (UIView *view in [_volumeView subviews]){
+        if ([view.class.description isEqualToString:@"MPVolumeSlider"]){
+            _volumeViewSlider = (UISlider*)view;
+            break;
+        }
+    }
+    [_volumeViewSlider setValue:0.0f animated:NO];
+}
+
+- (IBAction)Shop:(id)sender {
 }
 @end
